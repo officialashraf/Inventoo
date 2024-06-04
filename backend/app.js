3480ac9c6767cda9bbf26ee9e3d42af61c8a73f3
AshrafShaikh <113411746+officialashraf@users.noreply.github.com>
2024-06-04 12:24:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 const path = require("path");
 const cors = require("cors");
-const CLIENT_APP_PATH = '../frontend';
+//const CLIENT_APP_PATH = '../frontend';
 // config file coneection
 dotenv.config({path:"Backend/config/config.env"});
 
@@ -30,14 +30,14 @@ app.use("/api/v1", order);
 app.use("/api/v1",payment);
 
 
-app.use(express.static(CLIENT_APP_PATH));
+app.use(express.static(path));
 
 // ...express middlewares, rest, etc...
 
 // in the end of app routing
-// serves frontend application
+// serves frontend applicati
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(`${CLIENT_APP_PATH}/build/index.html`), { root: __dirname }, err => {
+    res.sendFile(path.resolve(../frontend/public/index.html), { root: __dirname }, err => {
         if (err) {
             res.status(500).send(err);
         }
